040af96cbd327e565e93c2ee2ff8780159700815
TUSHAR <tushar2525252@gmail.com>
2023-02-21 22:35:34 +0530

    Boiler Plate

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+const index = () => {
+  return (
+    <>
+    <h1 className="text-3xl text-white bg-red-500 font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default index
